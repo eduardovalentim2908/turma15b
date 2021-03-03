6b2d395ffa2a63e8abe781688dfdb0898a0c3c36
EduardoValentim <edu_valentim@uol.com.br>
2021-03-03 09:48:51 -0300

    App.java

--- aula07/exemplos/exemplo01/App.java
+++ aula07/exemplos/exemplo01/App.java
@@ -62,6 +62,5 @@ public class App {
         }
         System.out.println(cp1);
 
-
     }
 }
---
+++ aula07/exercicios/exercicio01/App.java
@@ -0,0 +1,7 @@
+package exercicios.exercicio01;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }    
+}
